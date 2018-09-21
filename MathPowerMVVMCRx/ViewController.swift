01a9431c96e6f139ebf269b19ade8d1bb51e19b5
Aleksandr Ivashchenko <aleksandr.ivashchenko@sigma.software>
2018-09-21 15:59:46 +0300

    Initial Commit

---
+++ MathPowerMVVMCRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathPowerMVVMCRx
+//
+//  Created by Aleksandr Ivashchenko on 9/21/18.
+//  Copyright © 2018 Aleksandr Ivashchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
